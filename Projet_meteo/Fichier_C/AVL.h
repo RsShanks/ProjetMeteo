43a888bdc0f617c5f050aa19106e90b13782c36f
Tytyaz2 <120456383+Tytyaz2@users.noreply.github.com>
2023-02-03 22:08:25 +0100

    Add files via upload

---
+++ Projet_meteo/Fichier_C/AVL.h
@@ -0,0 +1,31 @@
+#ifndef AVL_h
+#define AVL_h
+
+#include "ABR.h"
+
+#define MALLOC(x) ((x*)) malloc(sizeof(x))
+
+int max(int a, int b);
+int min(int a, int b);
+parbre Rotationdroite(parbre a);
+parbre Rotationgauche(parbre a);
+parbre doubleRotationDroite(parbre a);
+parbre doubleRotationGauche(parbre a);
+parbre inserert1(parbre a,double b1,double b2,double b3,double b4,double b5,double b6,int* h);
+parbre inserert2(parbre a,double b1,double b2,double b3,double b4,int* h);
+parbre inserert3(parbre a,double b1,double b2,double b3,double b4,double b5,double b6,int* h);
+parbre insererm(parbre a,double b1,double b2,double b3,double b4,double b5,double b6,int* h);
+parbre insererw(parbre a,double b1,double b2,double b3,double b4,double b5,double b6,int* h);
+parbre insererp1(parbre a,double b1,double b2,double b3,double b4,double b5,double b6,int* h);
+parbre insererh(parbre a,double b1,double b2,double b3,double b4,int* h);
+parbre fabricationAVLt1(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLt2(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLt3(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLt32(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLm(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLm2(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLw(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLp1(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+parbre fabricationAVLh(char *ligne, double b[],parbre a,int nbligne,int nbcolonne,FILE * fp,FILE * propre,int reverse);
+
+#endif
---
+++ Projet_meteo/Fichier_C/Filtre_ZONE.h
@@ -0,0 +1,12 @@
+#ifndef FILTRE_ZONE_h
+#define FILTRE_ZONE_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+void fabrication(char *ligne, double b[],int nbligne,int nbcolonne,FILE * fp,FILE * propre,int X_max,int X_min,int Y_max,int Y_min);
+
+#endif
